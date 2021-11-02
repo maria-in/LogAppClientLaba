944c9cef2987da351c27ee21f6c0e78c6f511dc9
Hanna Papova <gannapapova@gmail.com>
2021-11-02 12:43:38 +0300

    Edit user operation; Delete unnecessary

--- app/src/main/java/com/hannapapova/clientandroid/MainActivity.java
+++ app/src/main/java/com/hannapapova/clientandroid/MainActivity.java
@@ -1,17 +1,14 @@
 package com.hannapapova.clientandroid;
 
-import android.graphics.Color;
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -23,59 +20,38 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.text.SimpleDateFormat;
-import java.util.Base64;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final int SERVERPORT = 3003;
 
-    public static final String SERVER_IP = "192.168.0.100";
+    public static final String SERVER_IP = "192.168.100.223";
     private ClientThread clientThread;
     private Thread thread;
-    private LinearLayout msgList;
     private Handler handler;
-    private int clientTextColor;
 
     private TextInputEditText login;
     private TextInputEditText password;
     private TextInputEditText userInfoEdit;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         setTitle("Client");
-        clientTextColor = ContextCompat.getColor(this, R.color.green);
+        context = this;
         handler = new Handler();
-        msgList = findViewById(R.id.msgList);
-
         login = findViewById(R.id.userNameEditController);
         password = findViewById(R.id.passwordEditController);
         userInfoEdit = findViewById(R.id.userInfoController);
-
     }
 
-    public TextView textView(String message, int color) {
-        if (null == message || message.trim().isEmpty()) {
-            message = "<Empty Message>";
-        }
-        TextView tv = new TextView(this);
-        tv.setTextColor(color);
-        tv.setText(message + " [" + getTime() + "]");
-        tv.setTextSize(20);
-        tv.setPadding(0, 5, 0, 0);
-        return tv;
-    }
-
-    public void showMessage(final String message, final int color) {
+    public void showToast(String message) {
         handler.post(new Runnable() {
             @Override
             public void run() {
-                msgList.addView(textView(message, color));
+                Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
             }
         });
     }
@@ -83,69 +59,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onClick(View view) {
-
         if (view.getId() == R.id.connect_server) {
-            msgList.removeAllViews();
-            showMessage("Connecting to Server...", clientTextColor);
             clientThread = new ClientThread();
             thread = new Thread(clientThread);
             thread.start();
-            showMessage("Connected to Server...", clientTextColor);
             return;
         }
 
         if (view.getId() == R.id.logInButton) {
             String clientLogin = login.getText().toString().trim();
             String clientPassword = password.getText().toString().trim();
-
-            if (null != clientThread) {
+            if (clientThread != null) {
                 clientThread.sendMessage(authString(clientLogin, clientPassword));
             }
         }
 
-        if (view.getId() == R.id.editButton){
+        if (view.getId() == R.id.editButton) {
             String clientLogin = login.getText().toString().trim();
             String clientEditInfo = userInfoEdit.getText().toString().trim();
-
-            if (null != clientThread) {
+            if (clientThread != null) {
                 clientThread.sendMessage(editString(clientLogin, clientEditInfo));
             }
         }
     }
 
     class ClientThread implements Runnable {
-
         private Socket socket;
         private BufferedReader input;
 
         @Override
         public void run() {
-
             try {
                 InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
                 socket = new Socket(serverAddr, SERVERPORT);
 
                 while (!Thread.currentThread().isInterrupted()) {
-
                     this.input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     String message = input.readLine();
                     if (null == message || "Disconnect".contentEquals(message)) {
                         Thread.interrupted();
-                        message = "Server Disconnected.";
-                        showMessage(message, Color.RED);
                         break;
                     }
-                    showMessage("Server: " + message, clientTextColor);
+                    if (message.equals("auth error")) {
+                        showToast("Wrong password or username");
+                    } else {
+                        String operationWord = message.substring(0, message.indexOf(" "));
+                        if (operationWord.equals("auth")) {
+                            String userInfo = message.substring(message.lastIndexOf('{') + 1, message.lastIndexOf('}'));
+                            showToast("Successfully logged in");
+                            handler.post(() -> userInfoEdit.setText(userInfo));
+                        } else if (operationWord.equals("edit")) {
+                            showToast("Info saved");
+                        }
+                    }
                 }
-
-            } catch (UnknownHostException e1) {
-                e1.printStackTrace();
             } catch (IOException e1) {
                 e1.printStackTrace();
             }
-
         }
-
 
         void sendMessage(String message) {
             new Thread(() -> {
@@ -154,7 +125,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         PrintWriter out = new PrintWriter(new BufferedWriter(
                                 new OutputStreamWriter(socket.getOutputStream())),
                                 true);
-
                         out.println(message);
                     }
                 } catch (Exception e) {
@@ -162,12 +132,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             }).start();
         }
-
-    }
-
-    String getTime() {
-        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
-        return sdf.format(new Date());
     }
 
     @Override
@@ -179,12 +143,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public static String authString(String username, String password){
-        return "auth {" + username + "}{" + password + "}";
+    public static String authString(String username, String password) {
+        String authString = "auth {" + username + "}{" + password + "}";
+        return authString;
     }
 
-    public static String editString(String username, String editInfo){
-        return "edit {" + username + "}{" + editInfo + "}";
+    public static String editString(String username, String editInfo) {
+        String editString = "edit {" + username + "}{" + editInfo + "}";
+        return editString;
     }
 }
 
